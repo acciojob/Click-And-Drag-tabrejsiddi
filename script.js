b34653fe9d2572970841ec55025f5cd79d6ba809
tabrejsiddi <139753435+tabrejsiddi@users.noreply.github.com>
2026-03-03 14:37:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const slider = document.querySelector('.items');
 let isDown = false;
 let startX = 0;
 let scrollLeft = 0;
+slider.style.overflowX = 'scroll';
+if (slider.scrollWidth <= slider.clientWidth) {
+  slider.querySelectorAll('.item').forEach(item => {
+    item.style.minWidth = '300px';
+  });
+}
 
 slider.addEventListener('mousedown', (e) => {
-  if (e.which !== 1) return; // left click only
+  if (e.which !== 1) return;
 
   isDown = true;
   slider.classList.add('active');
